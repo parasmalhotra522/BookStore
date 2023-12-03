d094791efa5555268771d5de40bb7ef154be4045
parasmalhotra522@gmail.com <parasmalhotra522@gmail.com>
2023-12-02 21:54:44 -0500

    Stripe Api added hwile creating order

--- backend/models/Books.model.js
+++ backend/models/Books.model.js
@@ -9,6 +9,12 @@ const bookSchema = mongoose.Schema({
     bookDescription : {type:String},
     bookPrice:{type:Number, required:true},
     
+    // inventory : {mongoose.Schema.Types.ObjectId}
+    
+    // bookQuantity : {type:Number, required: true} 
+    // If purchased succesful then decrease the bookQuantity from inventory, if zero out of stock, set status unavailable
+    //
+
 });
 const Book = mongoose.model('Book', bookSchema);
 
--- backend/routes/order.routes.js
+++ backend/routes/order.routes.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 import Order from "../models/Orders.model.js";
 import Customer from "../models/Customer.model.js";
 import express from "express";
+import stripeLib from 'stripe';
 import { authenticateToken } from "../utils/auth.js";
+import Book from "../models/Books.model.js";
+const stripe = stripeLib('sk_test_51Nsr57IKWKaCzW6hlGDg0GHN7F3kktuIdlbJrI0wmbZCHbsxIDMSlJ1FtiRVWCBo2B8FaK72hD0WmIl5ODr5pIeK00rIrdekY6');
 
 const app = express.Router();
 
@@ -42,34 +45,67 @@ app.get("/customer/:id", authenticateToken, async(req, res) => {
 });
 
 
+const calculateAmount = (books) => {
+    console.log("I AM IN CALCULATE PAYMENT", books);
+    return 50;
+}
+
 
 // only logged in user can craete an order
-app.post("/customer/placeOrder/:id",authenticateToken, async(req, res) => {
+
+app.post("/customer/placeOrder/:id", authenticateToken, async (req, res) => {
     try {
-    const {books, orderOptions} = req.body;
-    // find the customer...
-    const customerId = req.params.id;
-    const customer = await Customer.findOne({_id:customerId});
-    console.log("Found the customer with the id", customer);
+      const { books, orderOptions, customerIdStripeAccount, payment_method } = req.body;
+      const customerId = req.params.id;
+      const customer = await Customer.findOne({ _id: customerId });
+  
+      if (!customer) {
+        return res.status(404).send({ message: `Customer with id ${customerId} doesn't exist. Please log in with correct credentials.` });
+      }
+  
+      // Calculate the order amount
+      const orderAmount = await calculateAmount(books);
+  
+      // Create paymentIntent and confirm it
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: orderAmount,
+        currency: 'usd',
+        customer: customerIdStripeAccount,
+        confirmation_method: "automatic",
+        confirm: true,
+        payment_method,
+        return_url:'https://localhost:3000'
+      });
+  
+      // Create order in the database
+      const booksForOrder = await Promise.all(books.map(async (book) => {
+        const bookDetails = await Book.findOne({ _id: book.book });
+        return {
+          book: bookDetails,
+          quantity: book.quantity,
+        };
+      }));
+  
+      const newOrder = await Order.create({
+        customerId: customerId,
+        books: booksForOrder,
+        orderOptions: orderOptions,
+        paymentIntentId: paymentIntent.id,
+      });
+  
+      // change the inventory.... 
+     
 
-    if(!customer) {
-        res.status(404).send({message: `Customer with id ${customerId} doesn't exist please log in with correct crendentials `});
+      res.status(201).send({
+        message: `New order with Order id ${newOrder._id} created successfully`,
+        newOrder,
+        confirmedPaymentIntent: paymentIntent,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ message: "Oops!! Something went Wrong" });
     }
-    const newOrder = await Order.create({
-        customerId:customerId,
-        books:books,
-        orderOptions:orderOptions
-    });
-    res.status(200).send({message: `New order with Order id ${newOrder._id} created successfully`,
-    newOrder
-});
-}   
-    catch(error) {
-        console.error(error);
-        res.status(500).send({message: "Oops!! Something went Wrong"});
-    }
-
-});
+  });
 
 
 
